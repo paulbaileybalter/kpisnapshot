7b814710d3efc2271f13feecb13e45444bb98cb1
paulbaileybalter <paul@balter.com.au>
2026-09-01 13:57:48 +1000

    Delete functions/api/session.js

--- functions/api/session.js
+++
@@ -1,6 +0,0 @@
-import { isAuthenticated, json } from "../_lib/auth.js";
-
-export async function onRequestGet({ request, env }) {
-  const authed = await isAuthenticated(request, env);
-  return json({ authenticated: authed });
-}
